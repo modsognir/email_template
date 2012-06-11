e7691ee34ad11a75a66d52a33008538c50d6ced4
Jared Fraser <xanatosredfern@gmail.com>
2012-06-11 21:59:58 +1000

    Correctly works with multiple mailers

--- lib/email_template.rb
+++ lib/email_template.rb
@@ -9,17 +9,17 @@ module EmailTemplate
 
   def all(mailers=ActionMailer::Base.subclasses)
     [mailers].flatten.map do |mailer|
-      files.map do |file|
+      files(mailer).map do |file|
         EmailTemplate::Template.new(file, mailer)
       end
     end.flatten
 	end
   
-  def template_path(mailer=ClinicalMailer)
+  def template_path(mailer)
     Pathname.new(mailer.template_root.to_s).join(mailer.to_s.underscore)
   end
 
-  def files(mailer=ClinicalMailer)
+  def files(mailer)
     template_path(mailer).entries.reject {|e| e.to_s.match(/\.\z|\A\_/).present? }
   end
 
